6491405abc44ddfe7d0fbc80c4f84ab100946ed0
Sajib Saha <sajibsaha859@gmail.com>
2023-03-29 07:24:21 +0530

    applying State

--- src/NumberCard.js
+++ src/NumberCard.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const NumberCard = function (props) {
-   
+   const number=props.number
     // TODO 3: Add the onClick handler to the div tag and assign it the value (cardClickHandler) which is passed down as props
     return (
-        <div id={props.id} className="card-container" style={{ color: (props.color), position: "relative",justifyContent:"center", transform:"inherit" }} onClick={()=>props.cardClickHandler(props.id)}>
-            <span>Click</span>
+        <div id={props.id} className="card-container"  style={{ color: (props.color), position: "relative",justifyContent:"center", transform:"inherit" }} onClick={props.cardClickHandler}>
+            <span>{number}</span>
         </div>
     )
 }
